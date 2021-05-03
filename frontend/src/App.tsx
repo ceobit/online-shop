f4ca90efa21483256b2ab7ec04a017550eb4c529
Roman Lebedenko <ceobit90@gmail.com>
2021-05-03 22:59:09 +0200

    added get started page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
-import Button from './components/Button/Button';
 import Start from './pages/Start/Start';
 
 function App() {
   return (
     <div className="App">
       <Start />
-      <Button title="Get started" size="sm" />
     </div>
   );
 }
--- frontend/src/pages/Start/Start.tsx
+++ frontend/src/pages/Start/Start.tsx
@@ -3,15 +3,22 @@ import classNames from 'classnames';
 
 import started_gif from '../../images/started.gif';
 import styles from './start.module.scss';
+import Button from '../../components/Button/Button';
 
 const Start = () => {
   const pageStyle = classNames(styles.started);
 
   return (
-    <div className={pageStyle}>
-      <img className={styles.started_gif} src={started_gif} alt="slides" />
-      <div className={styles.started_gif_cover}/>
-    </div>
+    <>
+      <div className={pageStyle}>
+        <img className={styles.started_gif} src={started_gif} alt="slides" />
+        <div className={styles.started_gif_cover} />
+        <div className={styles.started_shape} />
+      </div>
+      <div className={styles.started_bg}>
+        <Button title="Get started" size="sm" />
+      </div>
+    </>
   );
 };
 
