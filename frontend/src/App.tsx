0077734508a7c885dbd92838dfcc94912276bdbb
Roman Lebedenko <ceobit90@gmail.com>
2021-06-14 12:16:36 +0200

    added react router dom in order to switch between pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
 import './App.scss';
 import Start from './pages/Start/Start';
 import EnterPhone from './pages/EnterPhone/EnterPhone';
 import Iphone from './components/Iphone/Iphone';
+import { useRoutes } from './routes';
+import { history } from './history';
 
 function App() {
+  const routes = useRoutes();
+
   return (
     <div className="App">
       <Iphone />
-      <Start />
-      {/*<EnterPhone/>*/}
+      <Router history={history}>{routes}</Router>
+      {/*/!*<Start />*!/*/}
+      {/*<EnterPhone />*/}
     </div>
   );
 }
--- frontend/src/pages/EnterPhone/EnterPhone.tsx
+++ frontend/src/pages/EnterPhone/EnterPhone.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import PhoneInput from '../../components/PhoneInput/PhoneInput';
+import styles from './enterPhone.module.scss';
 
 const EnterPhone = () => {
   return (
-    <div>
+    <div className={styles.enterPhonePage}>
       <PhoneInput />
     </div>
   );
